64e6e0b9944d72e2dc42e44a1664598a1739fabe
Fares Elawady <ifareselawady50@gmail.com>
2025-10-16 22:00:05 +0300

    Add project files.

---
+++ api_test/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using api_test.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace api_test.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ api_test/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api_test.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
